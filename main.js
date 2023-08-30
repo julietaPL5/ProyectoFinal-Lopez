0d2375df7e2cfb7a8585b4d8e6c8b10a9a74e572
julietaPL5 <julietapilarlopez@gmail.com>
2023-08-30 18:09:29 -0300

    correcciones 1

--- main.js
+++ main.js
@@ -3,129 +3,44 @@
 const contenidoProductos = document.getElementById("contenidoProductos")
 const abrirCarrito = document.getElementById("abrirCarrito")
 const contenidoCarrito = document.getElementById("contenidoCarrito")
-
-
-const productos = [
-    {
-        id: 1,
-        nombre: "CD Fearless (Taylor's Version)",
-        precio: 6000,
-        imagen: "assets/images/fearless/cd.webp"
-    },
-    {
-        id: 2,
-        nombre: "Vinilo Fearless (Taylor's Version)",
-        precio: 13000,
-        imagen: "assets/images/fearless/vinilo.webp"
-    },
-    {
-        id: 3,
-        nombre: "Cassette Fearless (Taylor's Version)",
-        precio: 11000,
-        imagen: "assets/images/fearless/cassette.webp"
-    },
-    {
-        id: 4,
-        nombre: "CD Speak Now (Taylor's Version)",
-        precio: 7000,
-        imagen: "assets/images/speak-now/cd.webp"
-    },
-    {
-        id: 5,
-        nombre: "Vinilo Speak Now (Taylor's Version)",
-        precio: 14000,
-        imagen: "assets/images/speak-now/vinilo.webp"
-    },
-    {
-        id: 6,
-        nombre: "Cassette Speak Now (Taylor's Version)",
-        precio: 11500,
-        imagen: "assets/images/speak-now/cassette.webp"
-    },
-    {
-        id: 7,
-        nombre: "CD Red (Taylor's Version)",
-        precio: 5000,
-        imagen: "assets/images/red/cd.webp"
-    },
-    {
-        id: 8,
-        nombre: "Vinilo Red (Taylor's Version)",
-        precio: 13000,
-        imagen: "assets/images/red/vinilo.webp"
-    },
-    {
-        id: 9,
-        nombre: "CD 1989 (Taylor's Version)",
-        precio: 7000,
-        imagen: "assets/images/1989/cd.webp"
-    },
-    {
-        id: 10,
-        nombre: "Vinilo 1989 (Taylor's Version)",
-        precio: 13000,
-        imagen: "assets/images/1989/vinilo.webp"
-    },
-    {
-        id: 11,
-        nombre: "Cassette 1989 (Taylor's Version)",
-        precio: 11000,
-        imagen: "assets/images/1989/cassette.webp"
-    },
-    {
-        id: 12,
-        nombre: "CD Folklore",
-        precio: 5000,
-        imagen: "assets/images/folklore/cd-deluxe.webp"
-    },
-    {
-        id: 13,
-        nombre: "CD Evermore",
-        precio: 5500,
-        imagen: "assets/images/evermore/cd-deluxe.webp"
-    },
-    {
-        id: 14,
-        nombre: "CD Midnights",
-        precio: 5000,
-        imagen: "assets/images/midnights/cd-moonstone_blue2.webp"
-    },
-    {
-        id: 15,
-        nombre: "Vinilo Midnights",
-        precio: 12000,
-        imagen: "assets/images/midnights/vinilo-moonstone_blue.webp"
-    }
-]
+const contenidoCompra = document.getElementById("contenidoCompra")
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || []
 
-productos.forEach((producto) => {
-    let contenido = document.createElement("div")
-    contenido.className = "card"
-    contenido.innerHTML = `
-        <img src="${producto.imagen}" alt="${producto.nombre}">
-        <p class="nombre">${producto.nombre}</p>
-        <p class="precio">Precio: $${producto.precio}</p>
-    `;
-    contenidoProductos.append(contenido)
 
-    let agregarAlCarrito = document.createElement("button")
-    agregarAlCarrito.innerText = "Agregar al carrito"
-    agregarAlCarrito.className = "agregar-carrito"
+fetch ("productos.json")
+    .then (response => response.json())
+    .then (data => {
+        const productos = data.productos
+        
+        productos.forEach((producto) => {
+            let contenido = document.createElement("div")
+            contenido.className = "card"
+            contenido.innerHTML = `
+                <img src="${producto.imagen}" alt="${producto.nombre}">
+                <p class="nombre">${producto.nombre}</p>
+                <p class="precio">Precio: $${producto.precio}</p>
+            `;
+            contenidoProductos.append(contenido)
 
-    contenido.append(agregarAlCarrito)
+            let agregarAlCarrito = document.createElement("button")
+            agregarAlCarrito.innerText = "Agregar al carrito"
+            agregarAlCarrito.className = "agregar-carrito"
 
-    agregarAlCarrito.addEventListener("click", () => {
-        carrito.push({
-            id: producto.id,
-            imagen: producto.imagen,
-            nombre: producto.nombre,
-            precio: producto.precio
-        })
-        guardarLocalStorage()
+            contenido.append(agregarAlCarrito)
+
+            agregarAlCarrito.addEventListener("click", () => {
+                carrito.push({
+                    id: producto.id,
+                    imagen: producto.imagen,
+                    nombre: producto.nombre,
+                    precio: producto.precio
+                })
+                guardarLocalStorage()
+            })
+        });
     })
-});
+
 
 const verCarrito = () => {
     contenidoCarrito.innerHTML = ""
@@ -156,29 +71,30 @@ const verCarrito = () => {
             <img src="${producto.imagen}" alt="${producto.nombre}">
             <p class="nombre">${producto.nombre}</p>
             <p class="precio">$${producto.precio}</p>
+            <img src="assets/icons/x.svg" alt="x" class="eliminar-productos">
         `;
         contenidoCarrito.append(carritoContent);
 
-        let eliminarProductos = document.createElement("div")
-        eliminarProductos.innerHTML = `
-            <img src="assets/icons/x.svg" alt="x">
-        `
-        eliminarProductos.className = "eliminar-productos"
-        carritoContent.append(eliminarProductos)
-        eliminarProductos.addEventListener("click", eliminarDeCarrito)
+        let eliminar = carritoContent.querySelector(".eliminar-productos")
+        eliminar.addEventListener("click", () => {
+            eliminarDeCarrito(producto.id)
+        })
     })
 
     const total = carrito.reduce((acc, el) => acc + el.precio, 0)
     const totalCompra = document.createElement("div")
     totalCompra.className = "contenido-total"
-    totalCompra.innerHTML = `Total a pagar: $${total}`;
+    totalCompra.innerHTML = `
+        <p>Total a pagar: $${total}</p>
+        <button id="btnPagar"><a href="compra.html">Pagar</a></button>
+    `;
     contenidoCarrito.append(totalCompra)
 }  
 
 abrirCarrito.addEventListener("click", verCarrito)
 
-const eliminarDeCarrito = () => {
-    const encontrarID = carrito.find((element) => element.id)
+const eliminarDeCarrito = (id) => {
+    const encontrarID = carrito.find((element) => element.id === id)
     carrito = carrito.filter((carritoID) => {
         return carritoID !== encontrarID;
     });
@@ -191,3 +107,19 @@ const guardarLocalStorage = () => {
     localStorage.setItem("carrito", JSON.stringify(carrito))
 }
 
+
+const compra = () => {
+    let compraContent = document.createElement("div")
+    compraContent.className = "compra-content"
+    compraContent.innerHTML = `
+        <p>Finaliza tu compra</p>
+        
+        <label id="labelNombre" for="nombre">Nombre:</label>
+        <input type="text" name="Nombre" id="Nombre" placeholder="Nombre">
+
+        <label for="Correo">Correo:</label>
+        <input type="email" name="Correo" id="Correo" placeholder="Correo">
+
+    `;
+}
+contenidoCompra.append(compraContent)
